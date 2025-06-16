471d340f249c36a2c4c5ee645dfa6a90443042a5
Howard Fernando Del Angel Leija <ferdelangel92@gmail.com>
2025-06-16 17:27:12 -0500

    First commit

---
+++ numeroentero.cpp
@@ -0,0 +1,29 @@
+/*
+	Project:	numeroentero.
+	File:		numeroentero.cpp
+	Date:		May 30th, 2025.
+	Author:		(C) Software Engineer. Howard Fernando Del Angel Leija.
+	Contact:	ferdelangel92@gmail.com
+	Version:	1.0
+*/
+
+/*
+	Actividad: Escribir un programa que lea un entero,
+	lo multiplique por 2 y, a continuacion, lo escriba de nuevo en la pantalla.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int x;
+
+	int i = i * 2;
+
+	cout << " i = " << i << endl;
+	cin >> x;
+
+	return 0;
+}
